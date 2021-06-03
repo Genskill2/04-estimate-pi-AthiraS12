fa2532a4a0589940772aa9952d22fc827564b757
AthiraS12 <84625456+AthiraS12@users.noreply.github.com>
2021-06-03 17:25:55 +0400

    Solution

--- monte_carlo.c
+++ monte_carlo.c
@@ -2,15 +2,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-
 float mc_pi(int);
-
 float frandom() {
   long int q = random();
   float ret = (float)q/(float)RAND_MAX;
   return ret;
 }
-
 int main(void) {
   float pi0;
   float pi1;
@@ -23,12 +20,10 @@ int main(void) {
       printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
       abort();
     }
-
   if (fabs(pi0 - pi1) > 0.05) {
       printf("Two separate estimates %f and %f are too different.\n", pi0, pi1);
       abort();
   }
-
     
   for (int i=2000; i<5000; i++) {
     pi0 = mc_pi(i);
